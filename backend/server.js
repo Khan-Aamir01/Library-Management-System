eb8a64db98238217704c19c328808497f1f0bcaa
Khan-Aamir01 <aamirofficial32@gmail.com>
2024-07-05 17:10:08 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const connectDb = require('./config/db.js')
 const bookRoutes = require('./routes/bookRoutes.js')
+const userRoutes = require('./routes/userRoutes.js')
 
 
 const app = express();
@@ -11,6 +12,7 @@ connectDb();
 app.use(express.json()); // To get Req is JSON format
 
 app.use('/api',bookRoutes);
+app.use('/api',userRoutes);
 
 app.get('/',(req,res)=>{
     res.send('Home Route');
