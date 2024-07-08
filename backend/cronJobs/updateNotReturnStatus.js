2db75804c94f58212a38230cdc01c893d7ee2241
Khan-Aamir01 <aamirofficial32@gmail.com>
2024-07-08 15:53:28 +0530

    Minor Changes

--- backend/cronJobs/updateNotReturnStatus.js
+++ backend/cronJobs/updateNotReturnStatus.js
@@ -1,8 +1,9 @@
 const cron = require('node-cron');
 const Borrow = require('../models/borrow');
+const Fine = require('../models/fine');
 
 
-// Will Run once per hour
+// Will Run once per hour //'0
 const updateNotReturnStatus = cron.schedule('0 * * * *',async ()=>{
     console.log('Running cron job to update Not Return statuses...');
     try {
@@ -16,12 +17,24 @@ const updateNotReturnStatus = cron.schedule('0 * * * *',async ()=>{
         await Promise.all(notReturnBorrows.map(async borrow => {
             borrow.status = 'NotReturned';
             await borrow.save();
+            await createFine(borrow._id, borrow.userId, borrow.bookId); // This will create a new Fine
         }));
+        
 
         console.log(`${notReturnBorrows.length} borrows updated to NotReturned.`);
     } catch (error) {
         console.error('Error updating borrows:', error.message);
     }
 });
+
+// Function that create new Fine
+const createFine = async(borrowId,userId,bookId)=>{
+
+    await Fine.insertMany({
+        borrowId : borrowId,
+        userId : userId,
+        bookId : bookId,     
+    });
+}
 
 module.exports = updateNotReturnStatus;
